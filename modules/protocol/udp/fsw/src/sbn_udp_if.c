f05e3955deb1143002a56185192960f6e7dfca32
Ivan Perez <ivan.perezdominguez@nasa.gov>
2026-03-23 12:16:31 -0700

    Fix #121, Adjust UDP module to change of buffer size type.

--- modules/protocol/udp/fsw/src/sbn_udp_if.c
+++ modules/protocol/udp/fsw/src/sbn_udp_if.c
@@ -232,8 +232,9 @@ static SBN_Status_t PollPeer(SBN_PeerInterface_t *Peer)
 
 static SBN_Status_t Send(SBN_PeerInterface_t *Peer, SBN_MsgType_t MsgType, SBN_MsgSz_t MsgSz, void *Payload)
 {
-    int32 BufSz = MsgSz + SBN_PACKED_HDR_SZ, SentSz = 0;
-    uint8 Buf[BufSz];
+    size_t BufSz  = MsgSz + SBN_PACKED_HDR_SZ;
+    int32  SentSz = 0;
+    uint8  Buf[BufSz];
 
     SBN_UDP_Peer_t     *PeerData = (SBN_UDP_Peer_t *)Peer->ModulePvt;
     SBN_NetInterface_t *Net      = Peer->Net;
@@ -250,7 +251,7 @@ static SBN_Status_t Send(SBN_PeerInterface_t *Peer, SBN_MsgType_t MsgType, SBN_M
 
     SentSz = OS_SocketSendTo(NetData->Socket, Buf, BufSz, &PeerData->Addr);
 
-    if (SentSz < BufSz)
+    if ((SentSz < 0) || (SentSz < BufSz))
     {
         EVSSendErr(SBN_UDP_SOCK_EID,
                    "incomplete socket send, tried to send %d bytes, returned %d",
